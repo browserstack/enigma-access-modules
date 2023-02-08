cbb849dd8d932b39f3cd4508fceb70801c05af62
Harish Ved <ved.harish3@gmail.com>
2023-02-08 13:12:44 +0530

    refactor: UI fixes for the request AWS access
    AWS UI fixes to be functional when a user request for AWS access.

--- aws_access/access.py
+++ aws_access/access.py
@@ -1,25 +1,13 @@
-from django.shortcuts import render
 from django.template import loader
-from BrowserStackAutomation.settings import ACCESS_APPROVE_EMAIL
 from . import helpers, constants
 from Access.access_modules.base_email_access.access import BaseEmailAccess
 from bootprocess.general import emailSES
 import logging
-from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
 
 class AWSAccess(BaseEmailAccess):
-    def grant_owner(self):
-        return [ACCESS_APPROVE_EMAIL]
-
-    def revoke_owner(self):
-        return [ACCESS_APPROVE_EMAIL]
-
-    def access_mark_revoke_permission(self, access_type):
-        return [ACCESS_APPROVE_EMAIL]
-
     def email_targets(self, user):
         return [user.email] + self.grant_owner()
 
@@ -28,11 +16,10 @@ class AWSAccess(BaseEmailAccess):
         user,
         labels,
         approver,
-        request_id,
+        request,
         is_group=False,
         auto_approve_rules=None,
     ):
-        exception = ""
         label_desc = self.combine_labels_desc(labels)
         label_meta = self.combine_labels_meta(labels)
         for label in labels:
@@ -40,40 +27,67 @@ class AWSAccess(BaseEmailAccess):
                 user, label["account"], label["group"]
             )
 
-        if auto_approve_rules:
-            email_subject = (
-                "Access Granted: %s for access to %s for user %s.<br>Request has been"
-                " approved by %s. <br> Rules :- %s"
-                % (
-                    request_id,
-                    label_desc,
-                    user.email,
-                    approver,
-                    " ,".join(auto_approve_rules),
+            if not granted_access:
+                logger.error(
+                    "Something when wrong while adding %s to group %s: %s"
+                    % (user.email, label["group"], str(exception))
                 )
-            )
-        else:
-            email_subject = (
-                "Access Granted: %s for access to %s for user %s.<br>Request has been"
-                " approved by %s." % (request_id, label_desc, user.email, approver)
-            )
-        email_body = self._generateStringFromTemplate(
-            "aws_access/approved_email_template.html.j2",
-            **{
-                "request_id": request_id,
-                "approver": approver,
-                "user_email": user.email,
-                "access_desc": label_desc,
-                "access_meta": label_meta,
-            },
-        )
-        email_targets = [user.email]
+                return False
+
         try:
-            emailSES(email_targets, email_subject, email_body)
+            self.__send_approve_email(
+                auto_approve_rules,
+                request.request_id,
+                label_desc,
+                user,
+                approver,
+                label_meta,
+            )
         except Exception as e:
             logger.error(f"{self.tag()} Could not send email for error {str(e)}")
+            return False
 
-        return granted_access, exception
+        return True
+
+    def __send_approve_email(
+        self, auto_approve_rules, request_id, label_desc, user, approver, label_meta
+    ):
+        if auto_approve_rules:
+            email_subject = """
+                Access Granted: %s for access to %s for user %s. Rules :- %s""" % (
+                request_id,
+                label_desc,
+                user.email,
+                " ,".join(auto_approve_rules),
+            )
+        else:
+            email_subject = "Access Granted: %s for access to %s for user %s." % (
+                request_id,
+                label_desc,
+                user.email,
+            )
+
+        email_body = self._generateStringFromTemplate(
+            "aws_access/approved_email_template.html.j2",
+            request_id=request_id,
+            approver=approver,
+            user_email=user.email,
+            access_desc=label_desc,
+            access_meta=label_meta,
+        )
+        email_targets = self.email_targets(user)
+        emailSES(email_targets, email_subject, email_body)
+
+    def __send_revoke_email(self, user, request_id, label_desc):
+        email_targets = self.email_targets(user)
+        email_subject = "Revoke Request: %s for access to %s for user %s" % (
+            request_id,
+            label_desc,
+            user.email,
+        )
+        email_body = ""
+
+        emailSES(email_targets, email_subject, email_body)
 
     def get_label_desc(self, access_label):
         if access_label["action"] == constants.GROUP_ACCESS:
@@ -91,63 +105,59 @@ class AWSAccess(BaseEmailAccess):
 
     def combine_labels_meta(self, access_labels):
         if access_labels:
-            combined_meta = access_labels[0]
-            for label in access_labels[1:]:
+            combined_meta = {"action": "", "account": "", "group": ""}
+            for label in access_labels:
                 for key, value in label.items():
-                    combined_meta[key] += f", {value}"
+                    combined_meta[key] = str(combined_meta[key]) + f", {str(value)}"
             return combined_meta
         return dict()
 
     def access_request_data(self, request, is_group=False):
         return dict({"accounts": helpers.get_aws_accounts()})
 
-    def revoke(self, user, label):
-        logger.info(
-            f'[{datetime.now().strftime("%Y-%m-%d")}] [{self.tag()}] Revoke'
-            f" Started({user.email}) : {label}"
+    def revoke(self, user, label, request):
+        is_revoked, exception = helpers.revoke_aws_access(
+            user, label["account"], label["group"]
         )
-        is_revoked, exception = helpers.revoke_aws_access(user.user, label)
-        logger.info(
-            f'[{datetime.now().strftime("%Y-%m-%d")}] [{self.tag()}] Revoke'
-            f" Result({user.email}) : {is_revoked}"
-        )
-        return is_revoked, exception
 
-    def get_extra_fields(self):
-        return list()
+        if not is_revoked:
+            logger.error(
+                "Something went wrong while removing %s from %s: %s"
+                % (user.email, label["group"], str(exception))
+            )
+            return False
+
+        label_desc = self.get_label_desc(label)
+        try:
+            self.__send_revoke_email(user, request.request_id, label_desc)
+            return True
+        except Exception as e:
+            logger.error("Could not send email for error %s", str(e))
+            return False
 
     def validate_request(self, access_labels_data, request_user, is_group=False):
         valid_access_label_array = list()
         for access_label_data in access_labels_data:
-            if not access_label_data.get("action"):
+            if (
+                not access_label_data.get("action")
+                and access_label_data["action"] != constants.GROUP_ACCESS
+            ):
                 raise Exception(constants.ERROR_MESSAGES["valid_action_required"])
-            if access_label_data["action"] == constants.GROUP_ACCESS:
-                if not helpers.aws_account_exists(access_label_data.get("account")):
-                    raise Exception(constants.ERROR_MESSAGES["valid_account_required"])
-                if not helpers.aws_group_exists(access_label_data.get("group")):
-                    raise Exception(constants.ERROR_MESSAGES["valid_group_required"])
-            valid_access_label = {"data": access_label_data}
+            if not access_label_data.get("account") and not helpers.aws_account_exists(
+                access_label_data.get("account")
+            ):
+                raise Exception(constants.ERROR_MESSAGES["valid_account_required"])
+            if not access_label_data.get("group") and not helpers.aws_group_exists(
+                access_label_data["account"], access_label_data.get("group")
+            ):
+                raise Exception(constants.ERROR_MESSAGES["valid_group_required"])
+            valid_access_label = {
+                "action": access_label_data.get("action"),
+                "account": access_label_data.get("account"),
+                "group": access_label_data.get("group"),
+            }
             valid_access_label_array.append(valid_access_label)
         return valid_access_label_array
-
-    def fetch_access_approve_email(self, request, data):
-        context_details = {
-            "approvers": {
-                "primary": data["approvers"]["primary"],
-                "other": data["approvers"]["other"],
-            },
-            "requestId": data["requestId"],
-            "user": request.user,
-            "requestData": data["request_data"],
-            "accessType": self.tag(),
-            "accessDesc": self.access_desc(),
-            "isGroup": data["is_group"],
-        }
-        return str(
-            render(
-                request, "aws_access/accessApproveEmail.html", context_details
-            ).content.decode("utf-8")
-        )
 
     def fetch_access_request_form_path(self):
         return "aws_access/accessRequest.html"
