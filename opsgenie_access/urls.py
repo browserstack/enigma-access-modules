a5177805826363870380b3aae72b9a8190b8cd00
Sharad Sharma <sharad@browserstack.com>
2023-06-13 12:32:08 +0530

    "refactor: changing re_path to path"

--- opsgenie_access/urls.py
+++ opsgenie_access/urls.py
@@ -1,7 +1,7 @@
 """opsgenie url configuration"""
-from django.urls import re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    re_path(r"^api/v1/opsgenie/teams/$", views.get_opsgenie_team)
+    path("api/v1/opsgenie/teams/", views.get_opsgenie_team)
 ]
